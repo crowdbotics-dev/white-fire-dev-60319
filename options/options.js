c3b5315eb2eb6e277939399831aa742773eaa0f6
Crowdbotics <engineering@crowdbotics.com>
2023-01-31 05:44:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "white-fire-dev-60319",
+  url: "https://white-fire-dev-60319.botics.co",
+  api: "https://white-fire-dev-60319.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
